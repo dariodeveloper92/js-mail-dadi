d7a7a5d4a9c58e81f26ecf71f6c81d2ce2191298
Dario <dario.defrancisci92@gmail.com>
2021-07-16 16:53:31 +0200

    caricato mail

--- js/dadi.main.js
+++ js/dadi.main.js
@@ -1,3 +1,21 @@
-/*Esercizio: Gioco dei dadi-------------------------------------------------------------
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+//Esercizio: Gioco dei dadi-------------------------------------------------------------
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+var giocatore1 = prompt('Inserisci nome giocatore')
+giocatore1 = Math.max(Math.random() * 6) + 1;
+giocatorepc = Math.max(Math.random() * 6) + 1;  
+
+console.log(giocatore1)
+console.log(giocatore2)
+
+giocatore1 = document.getElementById(giocatore1);
+giocatore2 = document.getElementById(giocatore1);
+
+vincitore = 'vincitore';
+
+    
+     
+
+
+
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
--- js/mail.main.js
+++ js/mail.main.js
@@ -3,8 +3,10 @@
 var mail = prompt("Inserisci la tua email");
 console.log(mail);
 
-//controlla che sia nella lista di chi può accedere:
 
+
+//controlla che sia nella lista di chi può accedere:
+var listaMail = ['dario.defrancisci92@gmail.com', 'office@spotify.com'];
 
 //stampa un messaggio appropriato sull’esito del controllo:
-
+document.getElementById("mail").innerHTML = ' Accesso eseguito correttamente ' ;
