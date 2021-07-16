9894965778154c210b20ca20f9ddfa7c8509c0f4
Dario <dario.defrancisci92@gmail.com>
2021-07-16 13:46:41 +0200

    caricato prompt mail

--- js/mail.main.js
+++ js/mail.main.js
@@ -1,4 +1,10 @@
-/*Esercizio: Mail---------------------------------------------------------------------
-Chiedi all’utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.*/
+//Esercizio: Mail---------------------------------------------------------------------
+//Chiedi all’utente la sua email:
+var mail = prompt("Inserisci la tua email");
+console.log(mail);
+
+//controlla che sia nella lista di chi può accedere:
+
+
+//stampa un messaggio appropriato sull’esito del controllo:
+
